0d1ea67d5f9146943af600c694fe0ea3036d36cf
Alex <peretjatko@univ-brest.fr>
2021-03-09 09:51:12 +0100

    Factorisation du code dans les fichiers de fonctions

--- code_esp8266/FR_ESP8266_TEST/FR_ESP8266_TEST.ino
+++ code_esp8266/FR_ESP8266_TEST/FR_ESP8266_TEST.ino
@@ -1,6 +1,9 @@
 // Librairies pour le fonctionnement avec le Back office de TOCIO
 #include "Tocio.h"
 
+// Librairies contenant les fonctions pour lire les mesures physiques.
+#include "Functions.h"
+
 
 
 typedef struct {
@@ -12,8 +15,6 @@ typedef struct {
 
 
 
-
-const int pin_batt = A0; //ESP8266 analog PIN = A0, ici niveau_battrie(pin_batt)
 
 
 
@@ -62,46 +63,4 @@ void loop() {
 
   // Pause .................................................
   delay(60 * 1000); // 1 minutes
-}
-
-
-
-
-
-
-// --------------------------------------------------------------------------------
-/**
- * Renvoie le timestamp actuel sous forme d'integer.
- * @return integer
- */
-int rtc_timestamp() {
-  return 1615220068;
-}
-
-// --------------------------------------------------------------------------------
-/**
- * Renvoie le poind en gramme sous forme d'intger.
- * @return integer
- */
-int mesure_poid() {
-  int poid = 5000; //poid en grame ici = 5kg capteur de 0 a 20kg 
-
-  // code CZL635-20
-  return poid;
-}
-
-
-// --------------------------------------------------------------------------------
-/**
- * Renvoie le niveau de la batterie (de 0 à 1024).
- * @return integer
- */
-int niveau_battrie() {
-  // Lecture de la valeur analogique sur la pin de la batterie
-  int nv_batt = analogRead(pin_batt);
-  
-  Serial.print("niveau de la battrie = ");
-  Serial.print(nv_batt);
-
-  return nv_batt;
 }
--- code_esp8266/FR_ESP8266_TEST/Functions.h
+++ code_esp8266/FR_ESP8266_TEST/Functions.h
@@ -1,1 +1,41 @@
+const int pin_batt = A0; //ESP8266 analog PIN = A0, ici niveau_battrie(pin_batt)
 
+
+
+
+// --------------------------------------------------------------------------------
+/**
+ * Renvoie le timestamp actuel sous forme d'integer.
+ * @return integer
+ */
+int rtc_timestamp() {
+  return 1615220068;
+}
+
+// --------------------------------------------------------------------------------
+/**
+ * Renvoie le poind en gramme sous forme d'intger.
+ * @return integer
+ */
+int mesure_poid() {
+  int poid = 5000; //poid en grame ici = 5kg capteur de 0 a 20kg 
+
+  // code CZL635-20
+  return poid;
+}
+
+
+// --------------------------------------------------------------------------------
+/**
+ * Renvoie le niveau de la batterie (de 0 à 1024).
+ * @return integer
+ */
+int niveau_battrie() {
+  // Lecture de la valeur analogique sur la pin de la batterie
+  int nv_batt = analogRead(pin_batt);
+  
+  Serial.print("niveau de la battrie = ");
+  Serial.print(nv_batt);
+
+  return nv_batt;
+}
