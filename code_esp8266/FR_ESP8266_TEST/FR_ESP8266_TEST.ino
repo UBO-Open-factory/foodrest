e4921938fc6d022fa42bc8879a6662bef2dc83c8
ShyroWolf <shyrowolf@gmail.com>
2021-03-09 11:13:14 +0100

    complément fonction + ajout fonction + commentary
    - ajout du pin maping des indicateur visuel + capteur CZL635_20
    - complement fonction CZL635_20 capteur de poid
    - complement fonction RTC en cours
    - ajout lien preference carte firebeetle esp8266

--- code_esp8266/FR_ESP8266_TEST/FR_ESP8266_TEST.ino
+++ code_esp8266/FR_ESP8266_TEST/FR_ESP8266_TEST.ino
@@ -1,31 +1,33 @@
+/**
+ * librairie boad firebeetle esp8266 : fichier->preference->http://download.dfrobot.top/FireBeetle/package_firebeetle8266_index.json
+ */
+ 
 // Librairies pour le fonctionnement avec le Back office de TOCIO
 #include "Tocio.h"
 
 // Librairies contenant les fonctions pour lire les mesures physiques.
 #include "Functions.h"
 
-
-
+/**
+ * 
+ */
 typedef struct {
   char* ssid = "congres";
   char* password = "sufca!2019!dsiun";
   char* IDPoubelle = "FR_0001";
   int poidVide = 0;
 } Config;
-
-
-
-
+//---------------------------------------------------------------------------------------
 
 /**
- *
+ * innitialisation wifi + capteur
  */
 void setup() {
 // initialisation de la liason série (pour le moniteur)
   Serial.begin(115200);
   Serial.println("OK, let's go");
 
-// Connection WIFI -----------------------------------------------------------------------
+// Connection WIFI ----------------------------------------------------------------------
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
@@ -43,46 +45,25 @@ void setup() {
 //--------------------------------------------------------------------------------------
 
 /**
- * setup capteur CZL635_20
+ * boucle de mesure et d'envoie des donnée vers le backoffice
  */
-void setup_CZL635_20() {
-  Serial.println("HX711 calibration sketch");
-  Serial.println("Remove all weight from scale");
-  Serial.println("After readings begin, place known weight on scale");
-  Serial.println("Press + or a to increase calibration factor");
-  Serial.println("Press - or z to decrease calibration factor");
-
-  scale.begin(DOUT, CLK); //definition des pin
-  scale.set_scale();
-  scale.tare(); //Reset the scale to 0
-
-  long zero_factor = scale.read_average(); //Get a baseline reading
-  Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
-  Serial.println(zero_factor);
-}
-//--------------------------------------------------------------------------------------
-
-
-
-
-// *********************************************************************************
 void loop() {
   // Concatenation des mesures .............................
   String Mesures = "";
 
 // timestamp0 is the 'Timestamp' value from your sensor 'RTC_Timestamp' (as float)
-  int Mesures.concat(formatString(rtc_timestamp(), "10.0"));
+  Mesures.concat(formatString(rtc_timestamp(), "10.0"));
 
 // poid1 is the 'Poid' value from your sensor 'CZL635-20' (as float)
-  int Mesures.concat(formatString( mesure_poid(), "-5.0"));
+  Mesures.concat(formatString( mesure_poid(), "-5.0"));
 
 // valeur analogique(0 a 1023)2 is the 'Valeur analogique(0 à 1023)' value from your sensor 'CAN (1024)' (as float)
-  int Mesures.concat(formatString(niveau_battrie(), "4.0"));
+  Mesures.concat(formatString(niveau_battrie(), "4.0"));
 
 // Envoie des données vers TOCIO .........................
   sendDataInHTTPSRequest(Mesures);
 
   // Pause .................................................
-  delay(60 * 1000); // 1 minutes
+  delay(60 * 1000); // 1 minutes=(60 * 1000)
 }
-//--------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------
--- code_esp8266/FR_ESP8266_TEST/Functions.h
+++ code_esp8266/FR_ESP8266_TEST/Functions.h
@@ -1,9 +1,40 @@
-const int pin_batt = A0; //ESP8266 analog PIN = A0, ici niveau_battrie(pin_batt)
+#include <Wire.h>
+#include <SPI.h>
 
+/**
+ * capteur CZL635_20 [poids(kg/lbs)]
+*/
+#include "HX711.h"
+#define DOUT  D3
+#define CLK  D2
+HX711 scale;
+float calibration_factor = -7050; //-7050 worked for my 440lb max scale setup <-- tuto
+//---------------------------------------------------------------------------------------
 
+#define led_r D4 //led indicateur visuel rouge
+#define led_v D5 //led indicateur visuel vert
+#define pin_batt A0 //ESP8266 analog PIN = A0, ici niveau_battrie(pin_batt)
 
+/**
+ * setup capteur CZL635_20
+ */
+void setup_CZL635_20() {
+  Serial.println("HX711 calibration sketch");
+  Serial.println("Remove all weight from scale");
+  Serial.println("After readings begin, place known weight on scale");
+  Serial.println("Press + or a to increase calibration factor");
+  Serial.println("Press - or z to decrease calibration factor");
 
-// --------------------------------------------------------------------------------
+  scale.begin(DOUT, CLK); //definition des pin
+  scale.set_scale();
+  scale.tare(); //Reset the scale to 0
+
+  long zero_factor = scale.read_average(); //Get a baseline reading
+  Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
+  Serial.println(zero_factor);
+}
+//---------------------------------------------------------------------------------------
+
 /**
  * Renvoie le timestamp actuel sous forme d'integer.
  * @return integer
@@ -11,21 +42,35 @@ const int pin_batt = A0; //ESP8266 analog PIN = A0, ici niveau_battrie(pin_batt)
 int rtc_timestamp() {
   return 1615220068;
 }
+// --------------------------------------------------------------------------------------
 
-// --------------------------------------------------------------------------------
 /**
- * Renvoie le poind en gramme sous forme d'intger.
- * @return integer
+ * mesure capteur de poid
+ * balence CZL635_20
  */
 int mesure_poid() {
-  int poid = 5000; //poid en grame ici = 5kg capteur de 0 a 20kg 
+  //int poid = 5000; //poid en grame ici = 5kg capteur de 0 a 20kg 
+  //return poid;
+  scale.set_scale(calibration_factor); //Adjust to this calibration factor
 
-  // code CZL635-20
-  return poid;
+  Serial.print("Reading: ");
+  Serial.print(scale.get_units(), 1);
+  Serial.print(" lbs"); //Change this to kg and re-adjust the calibration factor if you follow SI units like a sane person
+  Serial.print(" calibration_factor: ");
+  Serial.print(calibration_factor);
+  Serial.println();
+
+  if(Serial.available())
+  {
+    char temp = Serial.read();
+    if(temp == '+' || temp == 'a')
+      calibration_factor += 10;
+    else if(temp == '-' || temp == 'z')
+      calibration_factor -= 10;
+  }
 }
+//---------------------------------------------------------------------------------------
 
-
-// --------------------------------------------------------------------------------
 /**
  * Renvoie le niveau de la batterie (de 0 à 1024).
  * @return integer
@@ -39,3 +84,4 @@ int niveau_battrie() {
 
   return nv_batt;
 }
+//---------------------------------------------------------------------------------------
