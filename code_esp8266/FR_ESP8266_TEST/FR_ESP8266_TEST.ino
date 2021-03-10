43956ae110e2a04b44b6960de50fdc604eff9b79
Alex <peretjatko@univ-brest.fr>
2021-03-10 09:34:33 +0100

    Suppression code innutil

--- code_esp8266/FR_ESP8266_TEST/FR_ESP8266_TEST.ino
+++ code_esp8266/FR_ESP8266_TEST/FR_ESP8266_TEST.ino
@@ -8,26 +8,15 @@ Configuration configLocale;
 // Librairies contenant les fonctions pour lire les mesures physiques.
 #include "Functions.h"
 
-/**
- *
- */
-typedef struct {
-  char* ssid = "congres";
-  char* password = "sufca!2019!dsiun";
-  char* IDPoubelle = "FR_0001";
-  int poidVide = 0;
-} Config;
-//---------------------------------------------------------------------------------------
 
-/**
- * innitialisation wifi + capteur
- */
+
+// ************************************************************************************
 void setup() {
-// initialisation de la liason série (pour le moniteur)
+  // initialisation de la liason série (pour le moniteur)
   Serial.begin(115200);
   Serial.println("OK, let's go");
 
-// Connection WIFI ----------------------------------------------------------------------
+  // Connection WIFI ----------------------------------------------------------------------
   WiFi.mode(WIFI_STA);
   WiFi.begin(configLocale.ssid, configLocale.password);
 
@@ -40,25 +29,28 @@ void setup() {
   Serial.println("Voila, c'est fait.");
   Serial.print("IP address: "); Serial.println(WiFi.localIP());
   Serial.print("MAC address: "); Serial.println(WiFi.macAddress());
+  
+  // Initialisation de la balance
   setup_CZL635_20();
+
+  // Initialisation de l'horloge
   setup_rtc_pcf8523();
 }
-//--------------------------------------------------------------------------------------
 
-/**
- * boucle de mesure et d'envoie des donnée vers le backoffice
- */
+
+
+// ************************************************************************************
 void loop() {
   // Concatenation des mesures .............................
   String Mesures = "";
 
-// timestamp0 is the 'Timestamp' value from your sensor 'RTC_Timestamp' (as float)
+  // timestamp0 is the 'Timestamp' value from your sensor 'RTC_Timestamp' (as float)
   Mesures.concat(formatString(rtc_timestamp(), "10.0"));
 
-// poid1 is the 'Poid' value from your sensor 'CZL635-20' (as float)
+  // poid1 is the 'Poid' value from your sensor 'CZL635-20' (as float)
   Mesures.concat(formatString( mesure_poid(), "-5.0"));
 
-// valeur analogique(0 a 1023)2 is the 'Valeur analogique(0 à 1023)' value from your sensor 'CAN (1024)' (as float)
+  // valeur analogique(0 a 1023)2 is the 'Valeur analogique(0 à 1023)' value from your sensor 'CAN (1024)' (as float)
   Mesures.concat(formatString(niveau_battrie(), "4.0"));
 
 
@@ -78,4 +70,3 @@ void loop() {
   // Pause .................................................
   delay(60 * 1000); // 10 secondes
 }
-//---------------------------------------------------------------------------------------
