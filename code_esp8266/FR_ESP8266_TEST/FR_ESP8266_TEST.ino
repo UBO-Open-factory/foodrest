d1c427d104e69da2c2ea208da88ae1238be2ce54
Alex <peretjatko@univ-brest.fr>
2021-03-17 15:06:08 +0100

    Allumage et extinction de la LED Rouge
    Mise à jour de l'algorythme pour prendre en compte l'allumage et l'extinction de la LED Rouge tout au long des mesure, envoies et sauvegarde des données.

--- code_esp8266/FR_ESP8266_TEST/FR_ESP8266_TEST.ino
+++ code_esp8266/FR_ESP8266_TEST/FR_ESP8266_TEST.ino
@@ -28,6 +28,7 @@ Configuration configLocale;
 // ************************************************************************************
 void setup() {
   pinMode(RED_LED_PIN, OUTPUT);
+
   // initialisation de la liaison série (pour le moniteur)
   Serial.begin(115200);
   TraceDebug("OK, let's go");
@@ -42,7 +43,7 @@ void setup() {
 
 
   // Lecture de la config à partir du fichier sur la carte SD -----------------------------
-  // ( renseigne le ssid, password, poid, IDPoubelle
+  // ( renseigne le ssid, password, poid, IDPoubelle, etc... )
   configLocale = lectureConfigurationFromSD();
   /*
     Serial.println("configLocale : ");
@@ -58,30 +59,92 @@ void setup() {
     TraceDebug("On entre en mode calibration d'usine.");
     calibrageUsine();
 
-    // Mode normal ------------------------------------------------------------------------
+
+    // Lecture des mesures ---------------------------------------------------------------------------
+    // (on est en mode normal, pas de paramétrage usine à faire)
   } else {
 
+    String Mesures = "";
+
+    // TIMESTAMP
+    String timeStamp = rtc_getTimestamp();
+    Mesures.concat(timeStamp);
 
 
-    // Connection au WIFI
+    // POID
+    // Allumage/Extinction de la LED rouge ____________________ ROUGE ON
+    digitalWrite(RED_LED_PIN, HIGH);
+
+    int poid = mesure_poid();
+    Mesures.concat(formatString(poid, "-5.0"));
+
+    // Allumage/Extinction de la LED rouge ____________________ ROUGE OFF
+    digitalWrite(RED_LED_PIN, LOW);
+
+    // FORCE DU WIFI
+    int rssi = 0;
+    String retourTOCIO = "";
+
+    // BATTERIE
+    int niveauBatteri = niveau_battrie();
+    Mesures.concat(formatString(niveauBatteri, "4.0"));
+
+
+
+
+
+    // Connection au WIFI ----------------------------------------------------------------------------
     ConnectionWifiEnabled = connectionWifi();
 
     // On a réussi à se connecter au WIFI
     if ( ConnectionWifiEnabled ) {
-      // Extinction de la LED rouge ----------------------------------------------------------- ROUGE OFF
+      // Extinction de la LED rouge __________________________ ROUGE OFF
       digitalWrite(RED_LED_PIN, LOW);
 
       TraceDebug("Voila, c'est fait.");
       TraceDebug("IP : " + WiFi.localIP().toString() );
       TraceDebug("MAC address: " + WiFi.macAddress() );
 
+      rssi = WiFi.RSSI();
+
+
+
+      // Envoie des données vers TOCIO ------------------------------------------------------------
+      if ( ConnectionWifiEnabled ) {
+        // Allumage/Extinction de la LED rouge _______________ ROUGE ON
+        digitalWrite(RED_LED_PIN, HIGH);
+
+        retourTOCIO = sendDataInHTTPSRequest( Mesures, configLocale );
+
+        // Allumage/Extinction de la LED rouge _______________ ROUGE OFF
+        digitalWrite(RED_LED_PIN, LOW);
+
+        if ( retourTOCIO != "ok") {
+          AfficheErreur("ERR (main)> ERREUR lors de l'envoie vers TOCIO. L'erreur renvoyée est :");
+          TraceDebug(retourTOCIO);
+
+        } else  {
+          TraceDebug("Envoie réussi");
+        }
+      }
+
+      // Pas de connection au Wifi
     } else {
-      // Allumage de la LED rouge ------------------------------------------------------------- ROUGE ON
+      // Allumage de la LED rouge ___________________________ ROUGE ON
       digitalWrite(RED_LED_PIN, HIGH);
     }
+    
+    // Ecriture dans le fichier -----------------------------------------------------------------
+    // Allumage/Extinction de la LED rouge __________________ ROUGE ON
+    digitalWrite(RED_LED_PIN, HIGH);
 
-    // Initialisation de la balance
-    //setup_CZL635_20();
+    Mesures = configLocale.IDPoubelle + "," + timeStamp + "," + String(poid) + "," + String(niveauBatteri) + "," + String(rssi) + "," + retourTOCIO;
+    SD_write_Mesure(Mesures);
+    TraceDebug("Ecriture dans le fichier");
+    TraceDebug("Mesures: " + Mesures);
+
+    // Allumage/Extinction de la LED rouge _________________ ROUGE OFF
+    digitalWrite(RED_LED_PIN, LOW);
   }
 }
 
@@ -90,71 +153,4 @@ void setup() {
 // ************************************************************************************
 void loop() {
 
-
-  // Lecture des mesures ---------------------------------------------------------------------------
-  String Mesures = "";
-
-  // TIMESTAMP
-  String timeStamp = rtc_getTimestamp();
-  Mesures.concat(timeStamp);
-
-
-  // POID
-  // Allumage/Extinction de la LED rouge ----------------------------------------------------------- ROUGE ON
-  digitalWrite(RED_LED_PIN, HIGH);
-
-  int poid = mesure_poid();
-  Mesures.concat(formatString(poid, "-5.0"));
-
-  // Allumage/Extinction de la LED rouge ----------------------------------------------------------- ROUGE OFF
-  digitalWrite(RED_LED_PIN, LOW);
-
-
-  // FORCE DU WIFI
-  String rssi = String(WiFi.RSSI());
-
-
-  // BATTERIE
-  int niveauBatteri = niveau_battrie();
-  Mesures.concat(formatString(niveauBatteri, "4.0"));
-
-
-  // Envoie des données vers TOCIO ------------------------------------------------------------
-  String retour;
-  if ( ConnectionWifiEnabled ) {
-    // Allumage/Extinction de la LED rouge ----------------------------------------------------------- ROUGE ON
-    digitalWrite(RED_LED_PIN, HIGH);
-
-    retour = sendDataInHTTPSRequest( Mesures, configLocale );
-
-    // Allumage/Extinction de la LED rouge ----------------------------------------------------------- ROUGE OFF
-    digitalWrite(RED_LED_PIN, LOW);
-
-    if ( retour != "ok") {
-      AfficheErreur("ERR (main)> ERREUR lors de l'envoie vers TOCIO. L'erreur renvoyée est :");
-      TraceDebug(retour);
-
-    } else  {
-      TraceDebug("Envoie réussi");
-    }
-  }
-
-
-
-  // Ecriture dans le fichier -----------------------------------------------------------------
-  // Allumage/Extinction de la LED rouge ----------------------------------------------------------- ROUGE ON
-  digitalWrite(RED_LED_PIN, HIGH);
-
-  Mesures = configLocale.IDPoubelle + "," + timeStamp + "," + String(poid) + "," + String(niveauBatteri) + "," + rssi + "," + retour;
-  SD_write_Mesure(Mesures);
-  TraceDebug("Ecriture dans le fichier");
-  TraceDebug("Mesures: " + Mesures);
-  
-  // Allumage/Extinction de la LED rouge ----------------------------------------------------------- ROUGE OFF
-  digitalWrite(RED_LED_PIN, LOW);
-
-
-
-  // Pause .................................................
-  delay(60 * 1000); // 10 secondes
 }
