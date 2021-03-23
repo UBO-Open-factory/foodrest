85f192c5283756ccd3e3c3504901d201687a4064
Alex <peretjatko@univ-brest.fr>
2021-03-23 15:23:07 +0100

    Optimisation de la gestion des message de debug

--- code_esp8266/FR_ESP8266_TEST/Functions.h
+++ code_esp8266/FR_ESP8266_TEST/Functions.h
@@ -138,36 +138,44 @@ void rtc_setup_pcf8523() {
 String rtc_getTimestamp() {
   rtc.begin();
   rtc.start();
-  
+
   DateTime now = rtc.now();
-  Serial.print(now.year(), DEC);
-  Serial.print('/');
-  Serial.print(now.month(), DEC);
-  Serial.print('/');
-  Serial.print(now.day(), DEC);
-  Serial.print(" (");
-  //Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-  Serial.print(") ");
-  Serial.print(now.hour(), DEC);
-  Serial.print(':');
-  Serial.print(now.minute(), DEC);
-  Serial.print(':');
-  Serial.print(now.second(), DEC);
-  Serial.println();
-  Serial.print(" since midnight 1/1/1970 = ");
-  Serial.print(now.unixtime());
-  Serial.print("s = ");
-  Serial.print(now.unixtime() / 86400L);
-  Serial.println("d");
-  Serial.println();
   /*
-        return String(now.unixtime());
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    //Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+    Serial.print(" since midnight 1/1/1970 = ");
+    Serial.print(now.unixtime());
+    Serial.print("s = ");
+    Serial.print(now.unixtime() / 86400L);
+    Serial.println("d");
+    Serial.println();
   */
-  
-  String date = String(now.day(), DEC) + "-" + String(now.month(), DEC) + "-" + String(now.year(), DEC);
-  String heure = String(now.hour(), DEC) + ":" + String(now.minute(), DEC) + ":" + String(now.second(), DEC);
+  return String(now.unixtime());
+/*
+  char jour[3]; sprintf(jour, "%02i", now.day());
+  char mois[3]; sprintf(mois, "%02i", now.month());
+  String date = String(jour) + "-" + String(mois) + "-" + String(now.year() );
+
+
+  char hour[3]; sprintf(hour, "%02i", now.hour());
+  char minute[3]; sprintf(minute, "%02i", now.minute());
+  char second[3]; sprintf(second, "%02i", now.second());
+  String heure = String(hour) + ":" + String(minute) + ":" + String(second);
 
   return date + heure;
+  */
 }
 
 
--- code_esp8266/FR_ESP8266_TEST/GestionErreur.h
+++ code_esp8266/FR_ESP8266_TEST/GestionErreur.h
@@ -13,9 +13,17 @@ void AfficheErreur(String chaine) {
 
 /**
    Affiche une ligne pour le debug (si c'est autorisé dans le fichier de settings).
-*/
+   @see : http://www.pinon-hebert.fr/Knowledge/index.php/ARDUINO_predefined_constants
+
 void TraceDebug(String chaine) {
   if ( configLocale.AfficheTraceDebug ) {
+    Serial.print(__FILE__); Serial.print(" ");
+    Serial.print(__DATE__); Serial.print(" ");
+    Serial.print(__TIME__); Serial.print(" ");
+    Serial.print(__func__); Serial.print(" ");
+    Serial.print(__LINE__); Serial.print(":");
     Serial.println( chaine );
   }
 }
+*/
+#define TraceDebug(message) Serial.print("DEBUG("); Serial.print(__FILE__); Serial.print(" "); Serial.print(__func__); Serial.print(","); Serial.print(__LINE__); Serial.print(")>"); Serial.println(message);
